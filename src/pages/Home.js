e968a36b2e71ddc0603b47ecb4924ed23931e122
khr-rakib <khr.rakib@gmail.com>
2020-11-20 16:32:45 +0600

    header footer part complete

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../components/Footer/Footer';
+import HeroArea from '../components/HeroArea/HeroArea';
+
+const Home = () => {
+    return (
+        <>
+            <HeroArea />
+            <br />
+            <Footer />
+        </>
+    );
+};
+
+export default Home;
